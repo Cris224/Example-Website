312cb6e2ee82e7eec7f9a0c3615dc998594a19ab
Cristofer <cristoferlorenzana@gmail.com>
2021-10-04 03:26:14 -0500

    picture sets won't appear together

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
+const gowPics = document.getElementById('gowPics');
+const codPics = document.getElementById('codPics');
+const r6Pics = document.getElementById('r6Pics');
 function toggleGowImgs(){
-  const gowPics = document.getElementById('gowPics');
-  if (gowPics.style.display === "none"){
-    gowPics.style.display = "";
+    if (gowPics.style.display === "none"){
+      gowPics.style.display = "";
+      r6Pics.style.display = "none";
+      codPics.style.display = "none";
+    }
+    else{
+      gowPics.style.display = "none";
   }
-  else{
-    gowPics.style.display = "none";
   }
-}
-function toggleCodImgs(){
-  const codPics = document.getElementById('codPics');
-  if (codPics.style.display === "none"){
-    codPics.style.display = "";
+  function toggleCodImgs(){
+    if (codPics.style.display === "none"){
+      codPics.style.display = "";
+      gowPics.style.display = "none";
+      r6Pics.style.display = "none"
+    }
+    else{
+      codPics.style.display = "none";
+    }
+    }
+    function toggleR6Imgs(){
+    if (r6Pics.style.display === "none"){
+      r6Pics.style.display = "";
+      gowPics.style.display = "none";
+      codPics.style.display = "none";
+    }
+    else{
+      r6Pics.style.display = "none";
+    }
   }
-  else{
-    codPics.style.display = "none";
-  }
-}
-function toggleR6Imgs(){
-  const r6Pics = document.getElementById('r6Pics');
-  if (r6Pics.style.display === "none"){
-    r6Pics.style.display = "";
-  }
-  else{
-    r6Pics.style.display = "none";
-  }
-}
+
+
+
+
+ // if (r6Pics.style.display = "") {
+ //   codPics.style.display ==="none";
+ //   gowPics.style.display === "none";
